9dc8c970c9dcd0433a1f61db5f71287abcee3774
AnastasiyaV <AnastasiyaV@c133h054.wless.reed.edu>
2022-04-16 15:45:17 -0700

    Updated mutate

--- chromosome.cc
+++ chromosome.cc
@@ -86,7 +86,7 @@ Chromosome::create_crossover_child(const Chromosome* p1, const Chromosome* p2,
 double
 Chromosome::get_fitness() const
 {
-  return 1 / (1 + calculate_total_distance());
+  return 1 / (1 + f());
 }
 
 // A chromsome is valid if it has no repeated values in its permutation,
