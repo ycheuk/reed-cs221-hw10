ad2c592b903acae144f69203f5f8928270d07acb
vaughnzaayer <95721454+vaughnzaayer@users.noreply.github.com>
2022-04-20 09:14:01 -0700

    Add files via upload
    - More work on Makefile and unit_test, u_t is currently throwing a segmentation fault when initializing two chromosomes

--- ch_unit_test.cc
+++ ch_unit_test.cc
@@ -11,7 +11,7 @@
 Cities* gen_random_cities() {
 	std::vector<std::pair<int, int>> city_list;
 	Cities::coord_t city;
-	for(int i; i < 5; i++){
+	for(int i; i < 20; i++){
 		city.first = std::rand() % 100;
 		city.second = std::rand() % 100;
 		city_list.push_back(city);
@@ -20,8 +20,10 @@ Cities* gen_random_cities() {
 	return(rtn_ptr);
 }
 
-void print_chromosome_ordering(const Chromosome* chromosome) {
-	auto ordering = chromosome -> get_ordering();
+void print_chromosome_ordering(Chromosome& chromosome) {
+	auto ordering = chromosome.get_ordering();
+
+	std::cout << "CITY ORDERING: ";
 
 	for(auto city : ordering) {
 		std::cout << city << " ";
@@ -40,22 +42,33 @@ int main() {
 
 	// Print out cities order
 
-	std::cout << "FIRST CITY\n" << *test_cities << std::endl;
+	std::cout << "CITIES\n" << *test_cities << std::endl;
 
 
 	// Create a new chromosome w/ test_cities
-
 	Chromosome* test_chromosome1 = new Chromosome(test_cities);
 	
 	// Look at current chromosome1 ordering
-	print_chromosome_ordering(test_chromosome1);
+	print_chromosome_ordering(*test_chromosome1);
 
-///////////////////////////////////////////////////////////////////////////////////
+
+
+	// Create a second chromosome w/ test_cities
+	Chromosome* test_chromosome2 = new Chromosome(test_cities);
+
+	// Look at current chromosome2 ordering
+	print_chromosome_ordering(*test_chromosome2);
+
+
+/////////////////////////////////////////////////////////////////////////////////
 	// Test total distance
 	std::cout << test_chromosome1 -> calculate_total_distance() << std::endl;
 
+	// Test mutation
+	test_chromosome1 -> mutate();
+	print_chromosome_ordering(*test_chromosome1);
+	std::cout << "FITNESS RATING: " << test_chromosome1 -> get_fitness() << std::endl;
 
-	delete(test_cities);
 
 	return 0;
 }
